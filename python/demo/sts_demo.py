3cb45287025f01bef576881ed7cc1d3f7d6b9b60
liuyongqing <815331793@qq.com>
2019-10-22 22:06:21 +0800

    更新python sts sdk用例
    更新python sts sdk用例

--- python/demo/sts_demo.py
+++ python/demo/sts_demo.py
@@ -23,8 +23,9 @@ if __name__ == '__main__':
         'bucket': 'example-1253653367',
         # 换成 bucket 所在地区
         'region': 'ap-guangzhou',
-        # 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
-        'allow_prefix': '*', 
+        # 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径
+        # 例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+        'allow_prefix': 'exampleobject', 
         # 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
         'allow_actions': [
             # 简单上传
