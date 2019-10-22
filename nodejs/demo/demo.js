cd17ff5d66560cfbc42e66c3c8f67ab7d84be41d
lewzylu <327874225@qq.com>
2019-10-22 21:56:44 +0800

    Update demo.js

--- nodejs/demo/demo.js
+++ nodejs/demo/demo.js
@@ -9,7 +9,7 @@ var config = {
     // 放行判断相关参数
     bucket: 'test-bucket-1253653367', // 换成你的 bucket
     region: 'ap-guangzhou', // 换成 bucket 所在地区
-    allowPrefix: '*' // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
+    allowPrefix: 'exampleobject' // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
 };
 
 
@@ -61,7 +61,7 @@ var config = {
         action: 'name/cos:PutObject',
         bucket: config.bucket,
         region: config.region,
-        prefix: '1.txt',
+        prefix: 'exampleobject',
     }];
     var policy = STS.getPolicy(scope);
     STS.getCredential({
