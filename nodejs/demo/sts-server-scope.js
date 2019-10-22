4f0f2dafcc30b1f242c33c6aa3dc1cc81e1edfbd
lewzylu <327874225@qq.com>
2019-10-22 21:57:37 +0800

    Update sts-server-scope.js

--- nodejs/demo/sts-server-scope.js
+++ nodejs/demo/sts-server-scope.js
@@ -12,7 +12,7 @@ var config = {
     // 放行判断相关参数
     bucket: 'test-1250000000', // 换成你的 bucket
     region: 'ap-guangzhou', // 换成 bucket 所在地区
-    allowPrefix: '*', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
+    allowPrefix: 'exampleobject', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
     // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
     allowActions: [
         // 简单上传
