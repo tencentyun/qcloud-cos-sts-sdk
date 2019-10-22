5606ab75bab7b970df65f71f8ab2a9c3f715a0d0
lewzylu <327874225@qq.com>
2019-10-22 21:47:43 +0800

    Update sts-server.js

--- nodejs/demo/sts-server.js
+++ nodejs/demo/sts-server.js
@@ -12,7 +12,7 @@ var config = {
     // 放行判断相关参数
     bucket: 'test-1250000000',
     region: 'ap-guangzhou',
-    allowPrefix: '*',
+    allowPrefix: 'exampleobject', // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
     // 简单上传和分片，需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
     allowActions: [
         // 简单上传
