de1b15abf9eb591601f3245d16ef2e73a52c4d2b
tiedu <dutie123@qq.com>
2019-10-22 21:47:14 +0800

    Update CosStsClientTest.java

--- java/src/test/java/com/tencent/cloud/CosStsClientTest.java
+++ java/src/test/java/com/tencent/cloud/CosStsClientTest.java
@@ -40,8 +40,8 @@ public class CosStsClientTest {
             // 换成 bucket 所在地区
             config.put("region", "ap-beijing-1");
 
-            // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的目录，例子：* 或者 a/* 或者 a.jpg
-            config.put("allowPrefix", "*");
+            // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+            config.put("allowPrefix", "exampleobject");
 
              // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
             String[] allowActions = new String[] {
