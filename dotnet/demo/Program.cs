df93077ef9ea753b224223d6794009cd09e887fa
shawnnqin <shawnnqin@tencent.com>
2022-04-15 14:10:00 +0800

    support sts assumeRole

--- dotnet/demo/Program.cs
+++ dotnet/demo/Program.cs
@@ -6,9 +6,9 @@ namespace demo
 {
     class Program
     {
-        static void Main(string[] args)
+        // 获取联合身份临时访问凭证 https://cloud.tencent.com/document/product/1312/48195
+        static void GetCredentialDemo()
         {
-
             string bucket = "examplebucket-1253653367";  // 您的 bucket
             string region = "ap-guangzhou";  // bucket 所在区域
             string allowPrefix = "exampleobject"; // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
@@ -58,6 +58,67 @@ namespace demo
             {
                 Console.WriteLine("{0} = {1}", kvp.Key, kvp.Value);
             }
+        }
+
+        static void GetRoleCredentialDemo()
+        {
+            string bucket = "examplebucket-1253653367";  // 您的 bucket
+            string region = "ap-guangzhou";  // bucket 所在区域
+            string allowPrefix = "exampleobject"; // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+            string[] allowActions = new string[] {  // 允许的操作范围，这里以上传操作为例
+                "name/cos:PutObject",
+                "name/cos:PostObject",
+                "name/cos:InitiateMultipartUpload",
+                "name/cos:ListMultipartUploads",
+                "name/cos:ListParts",
+                "name/cos:UploadPart",
+                "name/cos:CompleteMultipartUpload"
+            };
+            string roleArn = "qcs::cam::uin/12345678:roleName/testRoleName";
+            // Demo 这里是从环境变量读取，如果是直接硬编码在代码中，请参考：
+            // string secretId = "AKIDXXXXXXXXX";
+            string secretId = Environment.GetEnvironmentVariable("COS_KEY"); // 云 API 密钥 Id
+            string secretKey = Environment.GetEnvironmentVariable("COS_SECRET"); // 云 API 密钥 Key
+
+            Dictionary<string, object> values = new Dictionary<string, object>();
+            values.Add("bucket", bucket);
+            values.Add("region", region);
+            values.Add("allowPrefix", allowPrefix);
+            // 也可以通过 allowPrefixes 指定路径前缀的集合
+            // values.Add("allowPrefixes", new string[] {
+            //     "path/to/dir1/*",
+            //     "path/to/dir2/*",
+            // });
+            values.Add("allowActions", allowActions);
+            values.Add("durationSeconds", 1800);
+
+            values.Add("secretId", secretId);
+            values.Add("secretKey", secretKey);
+            values.Add("roleArn", roleArn);
+
+            // 设置域名
+            // values.Add("Domain", "sts.tencentcloudapi.com");
+
+            // Credentials = {
+            //   "Token": "4oztDXOAAI3c6qUE5TkNuVzSP1tUQz15f3f946eb08f9411d3d61505cc4bc74cczCZLchkvRmmrqzE09ELVw35gzYlBXsQp03PBpL79ubLvoAMWbBgSMmI6eApmhqv7NFeDdKJlikVe0fNCU2NNUe7cHrgttfTIK87ZnC86kww-HysFgIGeBNWpwo4ih0lV0z9a2WiTIjPoeDBwPU4YeeAVQAGPnRgHALoL2FtxNsutFzDjuryRZDK7Am4Cs9YxpZHhG7_F_II6363liKNsHTk8ONRZrNxKiOqvFvyhsJ-oTTUg0I0FT4_xo0lq5zR9yyySXHbE7z-2im4rgnK3sBagN47zkgltJyefJmaPUdDgGmvaQBO6TqxiiszOsayS7CxCZK1yi90H2KS3xRUYTLf94aVaZlufrIwntXIXZaHOKHmwuZuXl7HnHoXbfg_YENoLP6JAkDCw0GOFEGNOrkCuxRtcdJ08hysrwBw1hmYawDHkbyxYkirY-Djg7PswiC4_juBvG0iwjzVwE0W_rhxIa7YtamLnZJxQk9dyzbbl0F4DTYwS101Hq9wC7jtifkXFjBFTGRnfPe85K-hEnJLaEy7eYfulIPI9QiIUxi4BLPbzjD9j3qJ4Wdt5oqk9XcF9y5Ii2uQx1eymNl7qCA",
+            //   "TmpSecretId": "xxxxxxxxxxxx",
+            //   "TmpSecretKey": "PZ/WWfPZFYqahPSs8URUVMc8IyJH+T24zdn8V1cZaMs="
+            // }
+            // ExpiredTime = 1597916602
+            // Expiration = 2020/8/20 上午9:43:22
+            // RequestId = 2b731be1-ebe8-4638-8a72-906bc564a55a
+            // StartTime = 1597914802
+            Dictionary<string, object> credential = STSClient.getRoleCredential(values);
+            foreach (KeyValuePair<string, object> kvp in credential)
+            {
+                Console.WriteLine("{0} = {1}", kvp.Key, kvp.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GetCredentialDemo();
+            GetRoleCredentialDemo();
         }
     }
 }
--- dotnet/sdk/STSClient.cs
+++ dotnet/sdk/STSClient.cs
@@ -73,6 +73,85 @@ namespace COSSTS
             return dic;
         }
 
+        public static Dictionary<string, object> getRoleCredential(Dictionary<string, object> values) {
+            checkArguments(values, new string[] {"secretId", "secretKey", "region"});
+
+            Credential cred = new Credential {
+                SecretId = (string) values["secretId"],
+                SecretKey = (string) values["secretKey"]
+            };
+            string region = (string) values["region"];
+
+            ClientProfile clientProfile = new ClientProfile();
+            HttpProfile httpProfile = new HttpProfile();
+            String endpoint = values.ContainsKey("Domain") ? (string) values["Domain"]: 
+                "sts.tencentcloudapi.com";
+            httpProfile.Endpoint = endpoint;
+            clientProfile.HttpProfile = httpProfile;
+
+            // get policy
+            string policy = null;
+            if (values.ContainsKey("policy")) {
+                policy = (string) values["policy"];
+            }
+            if (policy == null) {
+                checkArguments(values, new string[] {"bucket", "allowActions"});
+                string bucket = (string) values["bucket"];
+                string[] allowActions = (string[]) values["allowActions"];
+                string[] allowPrefixes;
+                if (values.ContainsKey("allowPrefix")) {
+                    allowPrefixes = new string[] {(string) values["allowPrefix"]};
+                } else if (values.ContainsKey("allowPrefixes")) {
+                    allowPrefixes = (string[]) values["allowPrefixes"];
+                } else {
+                    throw new System.ArgumentException("allowPrefix and allowPrefixes are both null.");
+                }
+                policy = getPolicy(region, bucket, allowPrefixes, allowActions);
+            }
+
+            // duration
+            Int32 durationSeconds = 1800;
+            if (values.ContainsKey("durationSeconds")) {
+                durationSeconds = (Int32) values["durationSeconds"]; 
+            }
+
+            // RoleArn
+            string roleArn = null;
+            if (values.ContainsKey("roleArn")) {
+                roleArn = (string) values["roleArn"];
+            } else {
+                throw new System.ArgumentException("must have roleArn param in GetRoleCredential.");
+            }
+
+            // RuleSessionName
+            string roleSessionName = null;
+            if (values.ContainsKey("roleSessionName")) {
+                roleSessionName = (string) values["roleSessionName"];
+            } else {
+                roleSessionName = "cos-sts-sdk-dotnet";
+            }
+
+            Dictionary<string, object> body = new Dictionary<string, object>();
+            body.Add("DurationSeconds", durationSeconds);
+            body.Add("Name", "cos-sts-sdk-dotnet");
+            body.Add("RoleSessionName", roleSessionName);
+            body.Add("Policy", policy);
+            body.Add("RoleArn", roleArn);
+
+            StsClient client = new StsClient(cred, region, clientProfile);
+            AssumeRoleRequest req = new AssumeRoleRequest();
+
+            string strParams = JsonConvert.SerializeObject(body);
+            req = AssumeRoleRequest.FromJsonString<AssumeRoleRequest>(strParams);
+            AssumeRoleResponse resp = client.AssumeRoleSync(req);
+            string jsonString = JsonConvert.SerializeObject(resp);
+            Dictionary<string, object> dic = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+            if (dic.ContainsKey("ExpiredTime")) {
+                dic.Add("StartTime", Int32.Parse(dic["ExpiredTime"].ToString()) - durationSeconds);
+            }
+            return dic;
+        }
+
         private static void checkArguments(Dictionary<string, object> values, string[] args) {
             foreach (string arg in args) {
                 if (!values.ContainsKey(arg) || values[arg] == null) {
