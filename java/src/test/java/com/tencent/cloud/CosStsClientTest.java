7139efa72a9070a403b1e5553e6d09a8c9f8a1c9
jeffreykzli <jeffreykzli@tencent.com>
2023-01-09 20:57:48 +0800

    内网域名请求

--- java/src/test/java/com/tencent/cloud/CosStsClientTest.java
+++ java/src/test/java/com/tencent/cloud/CosStsClientTest.java
@@ -36,7 +36,7 @@ public class CosStsClientTest {
                 System.setProperty("https.proxyPort", properties.getProperty("https.proxyPort"));
             }
 
-            // 设置域名
+            // 设置域名,可通过此方式设置内网域名
             //config.put("host", "sts.internal.tencentcloudapi.com");
 
             // 临时密钥有效时长，单位是秒
