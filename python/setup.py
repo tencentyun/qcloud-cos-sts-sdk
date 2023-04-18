7984f6003020b9de668825a0b1e304d8e40d5704
l-iberty <2593093242@qq.com>
2023-04-18 11:56:49 +0800

    encoding utf8

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf8') as fh:
     long_description = fh.read()
 
 setup(
