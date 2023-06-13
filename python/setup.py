d0c128937584f073c5555d742e1f832d1182d1dc
l-iberty <2593093242@qq.com>
2023-06-13 17:43:44 +0800

    3.1.5 python2兼容性修复

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", "r", encoding='utf8') as fh:
 
 setup(
     name='qcloud-python-sts',
-    version='3.1.4',
+    version='3.1.5',
     description='this is sts for python on v3',
     long_description=long_description,
     long_description_content_type="text/markdown",
