674292e0d94a08f3e9ae0bf74b2368b755b9dccf
garenwang <garenwang@tencent.com>
2025-03-18 15:02:59 +0800

    fix

--- java/src/main/java/com/tencent/cloud/Response.java
+++ java/src/main/java/com/tencent/cloud/Response.java
@@ -7,14 +7,14 @@ public class Response {
     public long startTime;
     public long expiredTime;
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("RequestId:").append(requestId)
-                .append(", TmpSecretId:").append(credentials.tmpSecretId)
-                .append(", StartTime:").append(startTime)
-                .append(", ExpiredTime:").append(expiredTime)
-                .append(", Expiration:").append(expiration);
-        return sb.toString();
-    }
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("RequestId:").append(requestId)
+//                .append(", TmpSecretId:").append(credentials.tmpSecretId)
+//                .append(", StartTime:").append(startTime)
+//                .append(", ExpiredTime:").append(expiredTime)
+//                .append(", Expiration:").append(expiration);
+//        return sb.toString();
+//    }
 }
